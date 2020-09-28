ebfdfcbc1cae419417632b8659acad7d0ee68094
kevin <kevin.embry@hotmail.com>
2020-09-28 12:36:55 -0400

    Add video player

--- resources/js/components/videos/VideoModule.js
+++ resources/js/components/videos/VideoModule.js
@@ -1,21 +1,33 @@
 import React from 'react';
+import { VIDEOS } from '../../data/tjConstants';
 
 class VideoModule extends React.Component {
-    constructor() {
+    constructor(props) {
         super();
         this.state = {
             videoUrl: ""
         };
+
+        this.redirectToHome = this.redirectToHome.bind(this);
+    }
+
+    redirectToHome(e) {
+        this.props.history.push('/');
     }
 
     handleVideoChange(url) {
-        console.log(url);
         this.setState({videoUrl: url})
     }
 
     render() {
         return(
             <div className="videomodule">
+                <span 
+                    onClick={this.redirectToHome}
+                    className="close" 
+                    title="Close Decklogs"
+                >&times;
+                </span>
                 <div className="heading">
                     <h1>Thomas Jefferson Videos</h1>
                 </div>
@@ -23,24 +35,29 @@ class VideoModule extends React.Component {
                 <div className="siding">
                     <h3>Select Video To Play</h3>
                     <hr />
-                    <p><a href="#" onClick={() => this.handleVideoChange("https://www.youtube.com/embed/D3Kv_0MvoaI")}>USS Thomas Jefferson (SSBN-618)</a></p>
-                    <p><a href="#" onClick={() => this.handleVideoChange("https://www.youtube.com/embed/Jw9lN8WLeJ4")}>Submarine Service In The 1970's</a></p>
-                    <p><a href="#">Jefferson Link #3</a></p>
-                    <p><a href="#">Jefferson Link #4</a></p>
+                    {VIDEOS.map((video) => {
+                        return <div className="videoselect" key={video.url} onClick={() => this.handleVideoChange("https://www.youtube.com/embed/" + video.url)}>
+                                    <img className="videothumbs" src={"https://img.youtube.com/vi/" + video.url + "/1.jpg"} />
+                                    <p className="videodescription" >{video.description}</p>
+                                </div>
+                        })
+                    }
+                    
                 </div>
                 <div className="videoplayer">
+                    {this.state.videoUrl == "" && <img className="player" src="..\..\..\images\american-flag.jpg"></img>}
                     {this.state.videoUrl !== "" && <iframe 
                                                         className="player" 
                                                         src={this.state.videoUrl} 
-                                                        frameborder="1" 
+                                                        frameBorder="1" 
                                                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-                                                        allowfullscreen
+                                                        allowFullScreen
                                                     /> }
                 </div>
-
             </div>
         )
     }
+
 }
 
 export default VideoModule;
