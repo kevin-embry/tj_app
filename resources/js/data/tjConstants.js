253c39576926fda936320e0ba06e92a48b2f43b7
kevin <kevin.embry@hotmail.com>
2020-10-01 14:24:53 -0400

    Remove a few images. Change header image

--- resources/js/data/tjConstants.js
+++ resources/js/data/tjConstants.js
@@ -31,11 +31,19 @@ export const DIVISIONS = {
 
 export const SERVEDDATES = ["1962", "1963", "1964", "1965", "1966", "1967", "1968", "1969", "1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977", "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985"];
 
-//MAX 6 VIDEOS FOR NOW!!! >6 breaks flow of elements
+//MAX 6 VIDEOS FOR NOW!!! >6 breaks flow of elements *** I THINK I FIXED THIS???
 export const VIDEOS = [
     {url: "D3Kv_0MvoaI", description: "USS Thomas Jefferson (SSBN-618)"},
     {url: "Jw9lN8WLeJ4", description: "Submarine Service In The 1970's"},
     {url: "PuQO6IMCYVU", description: "Around The World In Polaris Nuclear Ballistic Missle Submarines"},
+    // {url: "PuQO6IMCYVU", description: "Around The World In Polaris Nuclear Ballistic Missle Submarines"},
+    // {url: "PuQO6IMCYVU", description: "Around The World In Polaris Nuclear Ballistic Missle Submarines"},
+    // {url: "PuQO6IMCYVU", description: "Around The World In Polaris Nuclear Ballistic Missle Submarines"},
+    // {url: "PuQO6IMCYVU", description: "Around The World In Polaris Nuclear Ballistic Missle Submarines"},
+    // {url: "PuQO6IMCYVU", description: "Around The World In Polaris Nuclear Ballistic Missle Submarines"},
+    // {url: "PuQO6IMCYVU", description: "Around The World In Polaris Nuclear Ballistic Missle Submarines"},
+    // {url: "PuQO6IMCYVU", description: "Around The World In Polaris Nuclear Ballistic Missle Submarines"},
+    // {url: "PuQO6IMCYVU", description: "Around The World In Polaris Nuclear Ballistic Missle Submarines"},
     // {url: "PuQO6IMCYVU", description: "Around The World In Polaris Nuclear Ballistic Missle Submarines"},
     // {url: "PuQO6IMCYVU", description: "Around The World In Polaris Nuclear Ballistic Missle Submarines"},
     // {url: "PuQO6IMCYVU", description: "Around The World In Polaris Nuclear Ballistic Missle Submarines"}
